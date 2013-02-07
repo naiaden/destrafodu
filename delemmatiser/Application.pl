29e8565da2211dbe3b6912c0a6fe5b7148f06af9
louis <louis@naiaden.nl>
2013-02-07 18:09:45 +0100

    Reading and writing is now fully functional. Command line arguments are now working as well.

--- delemmatiser/Application.pl
+++ delemmatiser/Application.pl
@@ -6,7 +6,7 @@ use Getopt::Std;
 
 binmode STDOUT, ":utf8";
 
-use vars qw( $opt_g $opt_r $opt_s );
+use vars qw( $opt_g $opt_e $opt_L $opt_p $opt_t $opt_T);
 
 require 'InfoExtractor.pl';
 require 'PersistenceFactory.pl';
@@ -15,62 +15,79 @@ require 'WeightingScheme.pl';
 
 
 # Mode: g p
-# 	-g				Generate lexicon files
-#   -p <dir>		Persistent mode, save files in directory
+# 	-g				Generate lexicon files						implemented
+#   -p <dir>		Persistent mode, save files in directory	implemented
 #
 # Inputs: [e|t] [l|L|T]
-#	-e <file>		Elex XML file
-# 	-l <file>		Lassy frequency counts file
-# 	-L <dir>   		Lassy XML directory
-#	-t <file>		Load persistent Elex file
-#	-T <file>		Load persistent Lassy file
-if( ! getopts('gl:p:' ) )
-{
-  die "You did something wrong. Yes. This is not really helping...\n";
-}
+#	-e <file>		Elex XML file								implemented
+# 	-l <file>		Lassy frequency counts file					
+# 	-L <dir>   		Lassy XML directory							implemented
+#	-t <file>		Load persistent Elex file					implemented
+#	-T <file>		Load persistent Lassy file					implemented
 
-#getopts('gl:p:' );
+
+#if( ! getopts('gl:p:' ) )
+#{
+#  die "You did something wrong. Yes. This is not really helping...\n";
+#}
+
+getopts('gl:p:e:L:t:T:' );
+
+
+our @trainData;
+our @testData;
 
 if( $opt_g )
 {
-	print "Generate, jeej!\n";	
+	print "Generating lexicon files\n";	
 	
-	if( $opt_l )
+	if( $opt_e )
 	{
-		#process_files($opt_l, \&readFile, $elexRegex);
-		#print "$opt_l\n";
+		print "Reading eLex XML file... ";
+		@trainData = extractElexXMLFile($opt_e);
+		
+		print "Number of entries read: ".(($#trainData+1)/4)."\n";
+		
+		if( $opt_p )
+		{
+			print "Saving eLex lexicon... ";
+			writeTrainLexicon("$opt_p/trainData.lexicon", \@trainData);
+			print "eLex lexicon saved in $opt_p/trainData.lexicon\n";
+		}
+	}
+	elsif( $opt_t )
+	{
+		print "Reading persistent eLex lexicon file... ";
+		@trainData = readTrainLexicon($opt_t, \@trainData);
+		
+		print "Number of entries read: ".(($#trainData+1)/4)."\n";
+		
 	}
 	
-/*	
-if( $opt_e )
+	if( $opt_L )
 	{
-		# elex
+		print "Reading Lassy XML directory... ";
+		@testData = extractLassyXMLDirectory($opt_L);
+		
+		print "Number of entries read: ".(($#testData+1)/3)."\n";
+		
+		if( $opt_p )
+		{
+			print "Saving Lassy lexicon... ";
+			writeTestLexicon("$opt_p/testData.lexicon", \@testData);
+			print "Lassy lexicon saved in $opt_p/testData.lexicon\n";
+		}
 	}
-*/
+	elsif( $opt_T )
+	{
+		print "Reading persistent lassy lexicon file... ";
+		@testData = readTestLexicon($opt_T, \@testData);
+		
+		print "Number of entries read: ".(($#testData+1)/3)."\n";
+		
+	}
+	
 }
-
-
-
-
-my @trainData = extractElexXMLFile("/tmp/elex");
-my @testData = extractLassyXMLDirectory("/home/louis/p1/delemmatiser/data/corpora/Lassy1.0/Treebank/dpc-kam-001286-nl-sen/");
-
-
-
-if( $opt_p )
-{
-	print "Making data persistent...";
-	writeTrainLexicon("$opt_p/trainData", \@trainData);
-	writeTestLexicon("$opt_p/testData", \@testData);
-}
-
-my @empty;
-readTrainLexicon("/tmp/trainData", \@empty);
-
-my @newTestData;
-@newTestData = readTestLexicon("/tmp/testData", \@newTestData);
-writeTestLexicon("/tmp/testData_new", \@newTestData);
-
 
 
 
